e96d5aa828ff18f7d4e620d76275dd085df0e2bb
joackim.orciere <joackim.orciere@alumni.univ-avignon.fr>
2020-12-03 10:37:29 +0100

    discussion is now bounded by aknowledgements

--- discussion.py
+++ discussion.py
@@ -1,5 +1,15 @@
 from references import *
 
+def findAknowledgementParagraph( text ):
+    text = text.split("\n\n")
+
+    i = len( text ) - 1
+    while( i >= 0 ):
+        if( re.search( 'Acknowledgments', text[i] ) or re.search( 'ACKNOWLEDGMENTS', text[i] ) ):
+                break
+        i += -1 
+    return i
+
 def findDiscussionParagraph( text ):
     text = text.split("\n\n")
 
@@ -8,6 +18,8 @@ def findDiscussionParagraph( text ):
         if( re.search( 'Discussion', text[i] ) or re.search( 'DISCUSSION', text[i] ) ):
                 break
         i += -1 
+    if( i <= 1 ):
+        return 99999
     return i
 
 
@@ -20,6 +32,9 @@ def getDiscussion(text):
 
         x = findDiscussionParagraph(text)
         y = findReferenceParagraph(text)
+        z = findAknowledgementParagraph(text)
+
+        y = min( y, z ) 
 
         text = text.split("\n\n")
 
